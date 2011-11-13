91a03fcf0ae4d795970f2dfd6d705720a6fda707
jpemberthy <jpemberthy@gmail.com>
2011-11-13 14:57:10 -0500

    transactions seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
 AdminUser.find_or_create_by_email("admin@jade.com",
                                   password: "password",
                                   password_confirmation: "password")
+
+
+names = [ "Federico Builes", "Diego Echeverri", "Juan Pemberthy", "Tony Montana",
+          "Michael Corleone", "Eddard Stark", "Robert Barratheon" ]
+
+100.times do
+  Transaction.create( date: 1.day.ago,
+                      amount: SecureRandom.random_number(25),
+                      user_name: names.sample,
+                      payment_method: Transaction::PAYMENT_OPTIONS.sample,
+                      last_four: Random.new.rand(1000..9999).to_s )
+end
