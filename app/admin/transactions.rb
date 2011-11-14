77cd6443e2cb2fc106ff3a57ab822335e66bf7e0
Federico Builes <federico.builes@gmail.com>
2011-11-14 11:33:24 -0800

    Adding no-op fraud action.

--- app/admin/transactions.rb
+++ app/admin/transactions.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register Transaction do
   config.sort_order = 'date_desc'
-  actions :index, :show
 
   filter :date
   filter :payment_method, as: :select, collection: Transaction::PAYMENT_OPTIONS
   filter :amount, as: :numeric
   filter :user_name
+
 
   index do
     column(:id) { |t| link_to t.id, [:admin, t] }
@@ -14,5 +14,8 @@ ActiveAdmin.register Transaction do
     column(:amount, sortable: :amount) { |t| number_to_currency(t.amount) }
     column :payment_method
     column "Last Four Digits", :last_four
+    column "Actions" do |transaction|
+      link_to "Mark as Fraud", ""
+    end
   end
 end
