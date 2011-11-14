2d06bec020f2b5e165a05ed64afee71077c4be22
Federico Builes <federico.builes@gmail.com>
2011-11-14 11:04:37 -0800

    Fixing the default order hack.

--- app/admin/transactions.rb
+++ app/admin/transactions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Transaction do
+  config.sort_order = 'date_desc'
+
   filter :date
   filter :payment_method, as: :select, collection: Transaction::PAYMENT_OPTIONS
   filter :amount, as: :numeric
@@ -7,20 +9,11 @@ ActiveAdmin.register Transaction do
   index do
     column(:id) { |t| link_to t.id, [:admin, t] }
     column :date
-    column :user_name
+    column "Client", :user_name
     column(:amount, sortable: :amount) { |t| number_to_currency(t.amount) }
     column :payment_method
-    column :last_four
+    column "Last Four Digits", :last_four
 
     default_actions
-  end
-
-  controller do
-    def index
-      # I know this is horrible but they don't support yet a default sort order.
-      # https://github.com/gregbell/active_admin/issues/352#issuecomment-2724249
-      params[:order] ||= "date_desc"
-      index!
-    end
   end
 end
